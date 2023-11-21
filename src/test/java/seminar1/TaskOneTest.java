9fc4bf29c79472965c7e2890b052bfa4fbb0bf37
Ruslan Karimov <ru-ka-@mail.ru>
2023-11-21 23:09:50 +0500

    create TaskOneTest -> added base anatations @Test

--- src/test/java/seminar1/TaskOneTest.java
+++ src/test/java/seminar1/TaskOneTest.java
@@ -1,2 +1,49 @@
-package seminar1;public class TaskOneTest {
+package seminar1;
+
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.assertEquals;
+
+// Фреймворк junit ищет все аннотации с @Test и проверяет их как UNIT тесты.
+// То, что происходит внутри методов с анностацией @Test, не отражается на работе программы.
+// Тесты не знают друг о друге.
+
+public class TaskOneTest {
+
+    @Test
+    public void testMethod() {
+        assertEquals(5, 2 + 3);
+    }
+
+    @Test
+    @BeforeEach
+    public void setup() {
+        // Инициализация ресурсов - если мы хотим, чтобы метод выполнялся перед каждым тестом.
+    }
+
+    @Test
+    @AfterEach
+    public void teardown() {
+        // Очистка ресурсов - если мы хотим, чтобы метод выполнялся после каждого теста.
+    }
+
+    @Test
+    @BeforeAll
+    public static void globalSetup() {
+        // Инициализация - если мы хотим, чтобы метод выполнялся один раз перед всеми тестами.
+    }
+
+    @Test
+    @AfterAll
+    public static void globalTeardown() {
+        // Очистка ресурсов - если мы хотим, чтобы метод выполнялся один раз после всех тестов.
+    }
+
+    @Test
+    @DisplayName("Тест суммирования")
+    public void testSum() {
+        assertEquals(5, 2 + 3);
+    }
+
+
 }
