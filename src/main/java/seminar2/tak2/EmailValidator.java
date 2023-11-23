ea3307cb854316f0b297bb0cd4721f7ff48311e9
Ruslan Karimov <ru-ka-@mail.ru>
2023-11-23 23:54:12 +0500

    added task2: tests from EmailValidator

--- src/main/java/seminar2/tak2/EmailValidator.java
+++ src/main/java/seminar2/tak2/EmailValidator.java
@@ -1,2 +1,15 @@
-package seminar2.tak2;public class EmailValidator {
+package seminar2.tak2;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
+    // Все символы в разном регистре, цифры от 0 до 9, знаки подчёркивания и дефиса,
+    // а также после символа @ может быть что угодно, но обязательно -> .+
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    public boolean isValidEmail(String email) {
+        return pattern.matcher(email).matches();
+    }
 }
--- src/test/java/seminar2/task2/EmailValidatorTest.java
+++ src/test/java/seminar2/task2/EmailValidatorTest.java
@@ -1,2 +1,37 @@
-package seminar2.task2;public class EmailValidatorTest {
+package seminar2.task2;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import seminar2.tak2.EmailValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EmailValidatorTest {
+    // Напишите тесты с использованием аннотаций @Test, @Before, @After
+    // @BeforeClass, @AfterClass для проверки различных сценариев,
+    // включая правильные и неправильные адреса.
+
+    private static EmailValidator emailValidator;
+
+    @BeforeAll
+    static void setUp() {
+        emailValidator = new EmailValidator();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        emailValidator = null;
+    }
+
+    @Test
+    void isValidEmail() {
+        assertTrue(emailValidator.isValidEmail("123kofa-ioh@mm"));
+    }
+
+    @Test
+    void isNotValidEmail() {
+        assertFalse(emailValidator.isValidEmail("123kofa-ioh"));
+    }
 }
