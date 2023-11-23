9fed97af0e680d4cf15b8511d485c54b2513f9a4
Ruslan Karimov <ru-ka-@mail.ru>
2023-11-23 19:36:44 +0500

    fix syntax

--- src/test/java/seminar1/TaskOneTest.java
+++ src/test/java/seminar1/TaskOneTest.java
@@ -15,25 +15,25 @@ public class TaskOneTest {
         assertEquals(5, 2 + 3);
     }
 
-    @Test
+
     @BeforeEach
     public void setup() {
         // Инициализация ресурсов - если мы хотим, чтобы метод выполнялся перед каждым тестом.
     }
 
-    @Test
+
     @AfterEach
     public void teardown() {
         // Очистка ресурсов - если мы хотим, чтобы метод выполнялся после каждого теста.
     }
 
-    @Test
+
     @BeforeAll
     public static void globalSetup() {
         // Инициализация - если мы хотим, чтобы метод выполнялся один раз перед всеми тестами.
     }
 
-    @Test
+
     @AfterAll
     public static void globalTeardown() {
         // Очистка ресурсов - если мы хотим, чтобы метод выполнялся один раз после всех тестов.
